c4117682d32579f1820d76f8a4f588f4c103d993
Sqazine <Sqazine@163.com>
2025-07-25 09:45:24 +0800

    separate GfxConfig

---
+++ Config/GfxConfig.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstdint>
+#include "Common.h"
+namespace CynicEngine
+{
+    enum class GfxBackend
+    {
+        VULKAN,
+        D3D12,
+        METAL,
+    };
+
+    enum class GfxBackBufferCount:uint8_t
+    {
+        ONE = 1,
+        TWO = 2,
+        THREE = 3,
+
+        MAX = THREE,
+    };
+
+    struct GfxConfig
+    {
+        GfxBackend backend{GfxBackend::VULKAN};
+        bool vSync{false};
+        GfxBackBufferCount backBufferCount{GfxBackBufferCount::TWO};
+#ifndef NDEBUG
+        bool enableGpuValidation{false};
+#endif
+    };
+}
